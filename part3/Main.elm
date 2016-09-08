89eb217a80f0f3ad700b83286eeeb9f68a21ee5f
Nick Balestra <nbalestra@opentable.com>
2016-09-08 23:07:13 +0100

    solution part3

--- part3/Main.elm
+++ part3/Main.elm
@@ -54,15 +54,18 @@ viewSearchResult result =
             [ text result.name ]
         , button
             -- TODO add an onClick handler that sends a DELETE_BY_ID msg
-            [ class "hide-result" ]
+            [ onClick { operation = "DELETE_BY_ID", data = result.id }, class "hide-result" ]
             [ text "X" ]
         ]
 
 
 update msg model =
     -- TODO if msg.operation == "DELETE_BY_ID",
-    -- then return a new model without the given ID present anymore.
-    model
+    if msg.operation == "DELETE_BY_ID" then
+        { model | results = List.filter (\item -> item.id /= msg.data) model.results }
+    else
+        -- then return a new model without the given ID present anymore.
+        model
 
 
 main =
